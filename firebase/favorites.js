d087715bbd3754d0fc405f9dc432994075f1c320
1itach1io <mrk581938@gmail.com>
2026-02-12 02:35:26 +0200

    Delete firebase directory

--- firebase/favorites.js
+++
@@ -1,6 +0,0 @@
-/* ==========================================
-   DISCOVER EGYPT - FIREBASE FAVORITES
-   للمستقبل / For future use
-   ========================================== */
-
-// User favorites logic will be added here
--- firebase/reviews.js
+++
@@ -1,6 +0,0 @@
-/* ==========================================
-   DISCOVER EGYPT - FIREBASE REVIEWS
-   للمستقبل / For future use
-   ========================================== */
-
-// User reviews logic will be added here
